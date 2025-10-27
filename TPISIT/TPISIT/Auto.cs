d6b639b5cb02289c20145611db5270dbe37ca043
unknown <francescoerrico87@gmail.com>
2025-10-27 13:38:26 +0100

    Veicolo

--- TPISIT/TPISIT/Auto.cs
+++ TPISIT/TPISIT/Auto.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TPISIT
+public class Auto : Veicolo
 {
-    internal class Auto
+    // Proprietà aggiuntiva specifica dell’auto
+    public int NumeroPorte { get; set; }
+
+    // Costruttore della classe derivata
+    public Auto(string marca, string modello, int anno, int numeroPorte)
+        : base(marca, modello, anno) // chiama il costruttore della classe base
     {
+        NumeroPorte = numeroPorte;
+    }
+
+    // Sovrascrive il metodo della classe base
+    public override void MostraInfo()
+    {
+        Console.WriteLine($"Auto: {Marca} {Modello}, Anno: {Anno}, Porte: {NumeroPorte}");
     }
 }
