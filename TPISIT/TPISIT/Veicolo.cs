0fee4083a75c9e3035cc211f1ae79f30968449b0
unknown <francescoerrico87@gmail.com>
2025-10-27 13:44:00 +0100

    Veicolo

--- TPISIT/TPISIT/Veicolo.cs
+++ TPISIT/TPISIT/Veicolo.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TPISIT
+public class Veicolo
 {
-    internal class Veicolo
+    // Proprietà della classe base
+    public string Marca { get; set; }
+    public string Modello { get; set; }
+    public int Anno { get; set; }
+
+    // Costruttore
+    public Veicolo(string marca, string modello, int anno)
     {
+        Marca = marca;
+        Modello = modello;
+        Anno = anno;
+    }
+
+    // Metodo della classe base
+    public virtual void MostraInfo()
+    {
+        Console.WriteLine($"Veicolo: {Marca} {Modello}, Anno: {Anno}");
     }
 }
